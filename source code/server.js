e1b1c7b2577aeab0e344d5241cd2f9dc64d23ee1
Tanvi <krishnamayitanvi@gmail.com>
2025-11-16 13:23:50 +0800

    Update server.js

--- source code/server.js
+++ source code/server.js
@@ -15,7 +15,7 @@ const pdfParse = require('pdf-parse');
 console.log('pdfParse type:', typeof pdfParse);
 
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const OPENROUTER_API_KEY = process.env.OPENROUTER_API_KEY;
 const MAX_FILE_SIZE_MB = 5; // 5MB max per file
 
@@ -111,3 +111,4 @@ app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 
 });
+
